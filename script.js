7ad17ced7a3b4eca2a937e6c43494a84f8d40443
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-29 16:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+// btn.addEventListener('click', () => {downloadImages(images);});
+
+function downloadImages(images){
+	const promises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = image.url;
+      img.alt = image.alt;
+      img.onload = () => {
+        resolve(img);
+      };
+      img.onerror = () => {
+        reject(`Failed to load image's URL: ${image.url}`);
+      };
+    });
+  });
+
+  Promise.all(promises)
+    .then(imgs => {
+      const output = document.getElementById('output');
+		output.innerHTML = null;
+      imgs.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+btn.addEventListener('click', () => {
+	downloadImages(images);
+});
